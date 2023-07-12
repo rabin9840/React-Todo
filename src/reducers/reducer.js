6fd03f54b444b0bde6d8945d2d8ffe2cd5652f2d
rabin9840 <rabinshrestha984156@gmail.com>
2023-07-12 19:37:56 +0545

    created validation schema for todo

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,31 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { actionTypes } from "../actions/actionTypes";
 
 const initialState={
     todos:[],
 };
 
-const todosSlice = createSlice({
-    name: "todos",
-    initialState,
-    reducers: {
-        addTodo: (state, action) => {
-            state.todos.push(action.payload);
-        },
-        fetchTodos: (state, action) => {
-            state.todos = action.payload;
-        },
-        deleteTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => todo._id !== action.payload);
-        },
-        updateTodo: (state, action) => {
-            state.todos = state.todos.map((todo) => todo._id === action.payload._id ? action.payload : todo);
-        },
-        sortTodo: (state) => {
-            state.todos.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
-        },
-    },
-});
+const todosReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_TODO:
+            return {
+                ...state,
+                todos: [...state.todos, action.payload],
+            };
+         case actionTypes.FETCH_TODOS:
+            return {
+                ...state,
+                todos: action.payload,
+            };
+        case actionTypes.DELETE_TODO:
+            return {
+                ...state,
+                todos: state.todos.filter((todo) => todo._id !== action.payload),
+            };
+        case actionTypes.UPDATE_TODO:
+            return {
+                ...state,
+                todos: state.todos.map((todo) => todo._id === action.payload._id ? action.payload : todo),
+            };
+        case actionTypes.SORT_TODOS:
+            return {
+                ...state,
+                todos:[...state.todos].sort((a,b)=>new Date(a.dueDate)- new Date(b.dueDate)),
+            }
+        default:
+            return state;
+    }
+}
 
-export const { addTodo, fetchTodos, deleteTodo, updateTodo, sortTodo } = todosSlice.actions;
-
-export default todosSlice.reducer;
+export default todosReducer;
