5fbbdb7ceabab40cb900311bb5ef7ed23d47513d
rabin9840 <rabinshrestha984156@gmail.com>
2023-07-14 11:57:06 +0545

    Created FirstTenTodos Components

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,27 +1,49 @@
 import Piechart from "./Piechart";
 // import Test from "./Test";
-import { useState } from "react";
+import axios from "axios";
+import FirstTenTodos from "../TodosList/FirstTenTodos";
 
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 const Dashboard = () => {
 	const [todosStatusData, setTodosStatusData] = useState([]);
 	const username = "12345678";
 	const password = "12345678";
+
+	useEffect(() => {
+		const getTodosStatusData = async () => {
+			try {
+				const response = await axios.get(
+					"http://localhost:3000/todos/todosCount",
+					{
+						headers: {
+							Authorization: `Basic ${btoa(`${username}:${password}`)}`,
+						},
+					}
+				);
+				console.log(response.data.data);
+				const todosStatusData = response.data.data;
+				setTodosStatusData(todosStatusData);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getTodosStatusData();
+	}, []);
+
 	const pieChartData = {
-		labels: ["Completed", "In Progress", "To Do"],
+		labels: todosStatusData.map((todosStatus) => todosStatus._id),
 		datasets: [
 			{
-				data: [30, 50, 20],
+				data: todosStatusData.map((todosStatus) => todosStatus.count),
 				backgroundColor: ["#36A2EB", "#FFCE56", "#FF6384"],
 			},
 		],
 	};
+
 	return (
 		<>
 			<h1>Dashboard</h1>
-			{/* <PieChart data={pieChartData} /> */}
-			{/* <PieChart /> */}
-			{/* <Test data={pieChartData} /> */}
+			<FirstTenTodos />
 			<Piechart data={pieChartData} />
 		</>
 	);
