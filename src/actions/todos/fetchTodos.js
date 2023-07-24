141a1ca7a27430414a0649b858c11b3dca44680e
rabin9840 <rabinshrestha984156@gmail.com>
2023-07-24 11:50:29 +0545

    updated fetch action for todosperpage

--- src/actions/todos/fetchTodos.js
+++ src/actions/todos/fetchTodos.js
@@ -1,16 +1,50 @@
 import axios from 'axios';
 import { actionTypes } from '../actionTypes';
 
-export const fetchTodos = (username, password) => {
+// export const fetchTodos = (username, password) => {
+//     return async (dispatch) => {
+//         try {
+//             const response = await axios.get("http://localhost:3000/todos", {
+//                 headers: {
+//                     Authorization: `Basic ${btoa(`${username}:${password}`)}`,
+//                 },
+//             });
+//             console.log(response.data.data);
+//             // const todos = response.data.data;
+//             const todos = response.data.data.docs;
+//             console.log(response.data.data.docs);
+//             todos.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
+
+//             dispatch({
+//                 type: actionTypes.FETCH_TODOS,
+//                 payload: todos,
+//             });
+//         }
+//         catch (error) {
+//             console.log(error);
+//         }
+//     }
+// }
+
+export const fetchTodos = (username, password, currentPage, todosPerPage, statusFilter) => {
     return async (dispatch) => {
         try {
+            const params = {
+                page: currentPage,
+                limit: todosPerPage,
+                status: statusFilter
+            };
             const response = await axios.get("http://localhost:3000/todos", {
                 headers: {
                     Authorization: `Basic ${btoa(`${username}:${password}`)}`,
                 },
+
+                params: params,
             });
             console.log(response.data.data);
-            const todos = response.data.data;
+            // const todos = response.data.data;
+            const todos = response.data.data.docs;
+            console.log(response.data.data.docs);
             todos.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
 
             dispatch({
--- src/components/TodosList/TodoItemsList.jsx
+++ src/components/TodosList/TodoItemsList.jsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react";
 import DeleteModal from "../Common Component/Modals/DeleteModal";
 import EditModal from "../Common Component/Modals/EditModal";
-import { Button, Tooltip, OverlayTrigger, Table } from "react-bootstrap";
+// import { Button, Tooltip, OverlayTrigger, Table } from "react-bootstrap";
+import { Tooltip } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchTodos } from "../../actions/todos/fetchTodos";
 import { deleteTodo } from "../../actions/todos/deleteTodo";
 import { updateTodo } from "../../actions/todos/updateTodo";
 import TodosTable from "../Common Component/Table/TodosTable";
+import ReactPaginate from "react-paginate";
 
 const TodoItems = () => {
 	const dispatch = useDispatch();
@@ -21,6 +23,9 @@ const TodoItems = () => {
 	const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
 	const [editTodo, setEditTodo] = useState({});
+	const [currentPage, setCurrentPage] = useState(1);
+	const [todosPerPage, setTodosPerPage] = useState(10);
+	const [statusFilter, setStatusFilter] = useState("todo");
 
 	const username = "12345678";
 	const password = "12345678";
@@ -50,9 +55,33 @@ const TodoItems = () => {
 	// const handleGetData = () => {
 	// 	dispatch(fetchTodos(username, password));
 	// };
+	const handlePageChange = (selectedPage) => {
+		setCurrentPage(selectedPage.selected + 1);
+	};
+
+	const handlePerPageChange = (e) => {
+		setTodosPerPage(parseInt(e.target.value));
+		// Optional: Fetch todos again with the updated todosPerPage value
+		// You may choose to update the todos immediately or wait for the user to apply filters.
+		// dispatch(fetchTodos(username, password, currentPage, parseInt(e.target.value), statusFilter));
+	};
+	// useEffect(() => {
+	// 	dispatch(fetchTodos(username, password));
+	// }, []);
+
+	const handleFilterChange = () => {
+		// Fetch filtered data based on current filters and page
+		dispatch(
+			fetchTodos(username, password, currentPage, todosPerPage, statusFilter)
+		);
+	};
+
 	useEffect(() => {
-		dispatch(fetchTodos(username, password));
-	}, []);
+		// Fetch initial data on component mount
+		dispatch(
+			fetchTodos(username, password, currentPage, todosPerPage, statusFilter)
+		);
+	}, [currentPage, todosPerPage, statusFilter, dispatch]);
 
 	const handleDelete = () => {
 		dispatch(deleteTodo(deleteTodoId, username, password));
@@ -89,6 +118,23 @@ const TodoItems = () => {
 	return (
 		<div className='todo-items'>
 			<h1>Todos</h1>
+			<div className='d-flex justify-content-start'>
+				<label className='pe-2'>Result per page</label>
+				{/* Step 3: Attach the event handler to update the state when the selection changes */}
+				<select
+					name='perPage'
+					className='rounded-2 select-per-page'
+					value={todosPerPage}
+					onChange={handlePerPageChange}
+				>
+					<option value='5'>5</option>
+					<option value='10'>10</option>
+					<option value='50'>50</option>
+					<option value='100'>100</option>
+					<option value='500'>500</option>
+				</select>
+			</div>
+
 			{/* <Table
 				responsive
 				hover
@@ -150,6 +196,12 @@ const TodoItems = () => {
 				renderTooltip={renderTooltip}
 				openEditModal={openEditModal}
 				openDeleteModal={openDeleteModal}
+			/>
+			<ReactPaginate
+				pageCount={Math.ceil(todos.length / todosPerPage)}
+				onPageChange={handlePageChange}
+				containerClassName='pagination'
+				activeClassName='active'
 			/>
 
 			<DeleteModal
