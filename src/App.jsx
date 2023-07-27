e660325b699ef9c26185a68f63193c18910ae5e1
rabin9840 <rabinshrestha984156@gmail.com>
2023-07-27 18:09:04 +0545

    fixed page-reloading bug in nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,62 @@
+// import "./App.css";
+// import { Route, Routes } from "react-router-dom";
+// import TodoListPage from "./pages/TodoListPage";
+// import DashboardPage from "./pages/DashboardPage";
+// import { Nav, Navbar, Container } from "react-bootstrap";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+
+// function App() {
+// 	return (
+// 		<div className='main-app-container'>
+// 			<Navbar
+// 				bg='dark'
+// 				variant='dark'
+// 				expand='lg'
+// 				className='navbar'
+// 			>
+// 				<Container className='nav-container'>
+// 					<Navbar.Brand href='/'>Todos</Navbar.Brand>
+// 					<Navbar.Toggle aria-controls='navbar-nav' />
+// 					<Navbar.Collapse id='navbar-nav'>
+// 						<Nav className='flex-column'>
+// 							<Nav.Link href='/'>Dashboard</Nav.Link>
+// 							<Nav.Link href='/todos'>Todos</Nav.Link>
+// 						</Nav>
+// 					</Navbar.Collapse>
+// 				</Container>
+// 			</Navbar>
+// 			<div className='content'>
+// 				<Routes>
+// 					<Route
+// 						path='/'
+// 						element={<DashboardPage />}
+// 					/>
+// 					<Route
+// 						path='/todos'
+// 						element={<TodoListPage />}
+// 					/>
+// 				</Routes>
+// 			</div>
+// 			<ToastContainer
+// 				position='top-right'
+// 				autoClose={3000}
+// 				hideProgressBar
+// 				newestOnTop={false}
+// 				closeOnClick
+// 				rtl={false}
+// 				pauseOnFocusLoss
+// 				draggable
+// 				pauseOnHover
+// 			/>
+// 		</div>
+// 	);
+// }
+
+// export default App;
+
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import TodoListPage from "./pages/TodoListPage";
 import DashboardPage from "./pages/DashboardPage";
 import { Nav, Navbar, Container } from "react-bootstrap";
@@ -16,12 +73,30 @@ function App() {
 				className='navbar'
 			>
 				<Container className='nav-container'>
-					<Navbar.Brand href='/'>Todos</Navbar.Brand>
+					{/* Use the Link component instead of anchor tag */}
+					<Link
+						to='/'
+						className='navbar-brand'
+					>
+						Todos
+					</Link>
 					<Navbar.Toggle aria-controls='navbar-nav' />
 					<Navbar.Collapse id='navbar-nav'>
 						<Nav className='flex-column'>
-							<Nav.Link href='/'>Dashboard</Nav.Link>
-							<Nav.Link href='/todos'>Todos</Nav.Link>
+							{/* Use the Link component instead of anchor tag */}
+							<Link
+								to='/'
+								className='nav-link'
+							>
+								Dashboard
+							</Link>
+							{/* Use the Link component instead of anchor tag */}
+							<Link
+								to='/todos'
+								className='nav-link'
+							>
+								Todos
+							</Link>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
