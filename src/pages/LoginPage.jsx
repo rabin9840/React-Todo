7cb1d5e3beca5e5710eca279ed258b472298607f
rabin9840 <rabinshrestha984156@gmail.com>
2023-07-31 11:14:58 +0545

    created login and session

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,9 +2,12 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import loginSchema from "../validation/loginSchema";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { useHistory } from "react-router-dom";
 
 const LoginPage = () => {
-	const initialLoginState = { email: "", password: "" };
+	const history = useHistory();
+
+	// const initialLoginState = { email: "", password: "" };
 	const handleSubmit = async (values, { setSubmitting }) => {
 		// Handle form submission here (e.g., call login API)
 		try {
@@ -14,22 +17,22 @@ const LoginPage = () => {
 				values
 			);
 			console.log(login_response);
-			if (login_response && login_response === 200) {
+			if (login_response && login_response.status === 200) {
 				toast.success("Login successful", { autoClose: 3000 });
 			}
+			history.push("/todos");
 		} catch (error) {
 			console.log(error);
 			console.log(error.response.data.message);
 			toast.warning(error.response.data.message, { autoClose: 3000 });
 		}
-
 		setSubmitting(false);
 	};
 
 	return (
 		<Formik
-			// initialValues={{ email: "", password: "" }}
-			initialValues={initialLoginState}
+			initialValues={{ email: "", password: "" }}
+			// initialValues={initialLoginState}
 			validationSchema={loginSchema}
 			onSubmit={handleSubmit}
 		>
